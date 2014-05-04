67faba356259d3bc10acfeb608dd3783c78f3cb6
helenemartin <helenemartin13@hotmail.com>
2014-05-04 23:16:20 +0100

    add test unit, stylesheets, logo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FilmwerkplaatsVoorraad::Application.config.secret_key_base = '01c5c42aa2a301bde711ea9906581deb4b2abcc84c2c14f0810b5318b483be5587b024a48e4d831109c2536be193e293f15cc2b2ff5ab2c347422f52ac485961'
